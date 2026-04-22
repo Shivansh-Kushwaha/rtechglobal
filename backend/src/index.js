85dd74c84a65ccc3ee687f1be1f1c8a67c6fa61d
Shivansh-Kushwaha <supershivanshkushwaha@gmail.com>
2026-04-22 22:42:54 +0530

    Project Complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ import cors from "cors"
 import path from "path";
 
 
-dotenv.config();
+dotenv.config({ path: "./.env" });
 const app=express();
 const PORT=process.env.PORT;
 const __dirname = path.resolve();
@@ -38,9 +38,9 @@ app.use("/webinfo",webInfoRoutes);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
-    })
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT ,()=>{
